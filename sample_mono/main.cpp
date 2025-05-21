168ef891d9e8de13aba210dedfd4e73bca426488
Kwang Baik <kwang@presien.com>
2025-05-21 16:10:52 +1000

    Works up to this point. No gstreamer

--- sample_mono/main.cpp
+++ sample_mono/main.cpp
@@ -2,29 +2,100 @@
 #include <time.h>
 #include <unistd.h>
 #include <atomic>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <signal.h>
+#include <csignal>
 #include "ArgusCapture.hpp"
 #include "opencv2/opencv.hpp"
 
-int main(int argc, char *argv[]) {
+// Global flag to indicate when the program should terminate
+static std::atomic<bool> g_terminate(false);
 
-    int camera_id_0 = 0;
+// Signal handler for graceful termination
+void signal_handler(int signal) {
+    if (signal == SIGINT || signal == SIGTERM) {
+        std::cout << "\nReceived termination signal. Shutting down gracefully..." << std::endl;
+        g_terminate.store(true);
+    }
+}
+
+// Thread function to handle a single camera
+void cameraThread(int camera_id, int rq_width, int rq_height, int rq_fps) {
+    // Create configuration for the camera
+    oc::ArgusCameraConfig config;
+    config.mDeviceId = camera_id;
+    config.mFPS = rq_fps;
+    config.mWidth = rq_width;
+    config.mHeight = rq_height;
+    config.verbose_level = 3;
+    config.hdr = false;
+
+    // Open the camera
+    oc::ArgusBayerCapture camera;
+    oc::ARGUS_STATE state = camera.openCamera(config);
+    if (state != oc::ARGUS_STATE::OK) {
+        std::cerr << "Camera " << camera_id << ": Failed to open Camera, error code " << ARGUS_STATE2str(state) << std::endl;
+        return;
+    }
+
+    std::cout << "Camera " << camera_id << ": Camera opened successfully with resolution: " 
+              << camera.getWidth() << "x" << camera.getHeight() 
+              << ", channels: " << camera.getNumberOfChannels() << std::endl;
+
+    cv::Mat rgb_frame;
+    rgb_frame = cv::Mat(camera.getHeight(), camera.getWidth(), CV_8UC4, 1);
+
+    int image_count = 0;
+    
+    while (!g_terminate.load()) {
+        if (camera.isNewFrame()) {
+            memcpy(rgb_frame.data, camera.getPixels(),
+                   camera.getWidth() * camera.getHeight() *
+                   camera.getNumberOfChannels());
+
+            // Save the image as JPEG with camera_id and frame number in the filename
+            std::string filename = "camera_" + std::to_string(camera_id) + 
+                                  "_frame_" + std::to_string(image_count) + ".jpg";
+            
+            std::vector<int> compression_params;
+            compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
+            compression_params.push_back(95); // JPEG quality (0-100)
+            
+            cv::imwrite(filename, rgb_frame, compression_params);
+            
+            std::cout << "Camera " << camera_id << ": Saved " << filename << std::endl;
+            image_count++;
+        }
+        else {
+            usleep(100);
+        }
+    }
+
+    camera.closeCamera();
+    std::cout << "Camera " << camera_id << ": Camera closed" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    // Set up signal handler for graceful termination
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+
     int rq_width=0;
     int rq_height=0;
     int rq_fps = 0;
 
-    if (argc > 1) camera_id_0 = atoi(argv[1]);
-    if (argc > 2) rq_width = atoi(argv[2]);
-    if (argc > 3) rq_height = atoi(argv[3]);
-    if (argc > 4) rq_fps = atoi(argv[4]);
-
-
-
+    if (argc > 1) rq_width = atoi(argv[1]);
+    if (argc > 2) rq_height = atoi(argv[2]);
+    if (argc > 3) rq_fps = atoi(argv[3]);
     int major, minor, patch;
     oc::ArgusVirtualCapture::getVersion(major, minor, patch);
-    std::cout << " Argus Capture Version : " << major << "." << minor << "." << patch << std::endl;
+    std::cout << "Argus Capture Version: " << major << "." << minor << "." << patch << std::endl;
 
-
+    // Get all Argus devices
     std::vector<oc::ArgusDevice> devs = oc::ArgusBayerCapture::getArgusDevices();
+    std::cout << "Found " << devs.size() << " Argus devices:" << std::endl;
     for (int i = 0; i < devs.size(); i++) {
         std::cout << "##################" << std::endl;
         std::cout << " Device : " << devs.at(i).id << std::endl;
@@ -34,56 +105,36 @@ int main(int argc, char *argv[]) {
     }
     std::cout << "***********************" << std::endl;
 
-    /// Create configuration for the camera
-    oc::ArgusCameraConfig config;
-    config.mDeviceId = (camera_id_0);
-    config.mFPS = rq_fps;
-    config.mWidth = rq_width;
-    config.mHeight = rq_height;
-    config.verbose_level = 3;
-    config.hdr = false;
-
-    /// Open the camera
-    oc::ArgusBayerCapture camera_0;
-    oc::ARGUS_STATE state_cam0 = camera_0.openCamera(config);
-    if (state_cam0 != oc::ARGUS_STATE::OK) {
-        std::cerr << "Failed to open Camera, error code " << ARGUS_STATE2str(state_cam0) << std::endl;
+    // Find available cameras
+    std::vector<int> available_camera_ids;
+    for (const auto& dev : devs) {
+        if (dev.available) {
+            available_camera_ids.push_back(dev.id);
+        }
+    }
+    
+    if (available_camera_ids.empty()) {
+        std::cerr << "No available cameras found!" << std::endl;
         return -1;
     }
-
-    cv::Mat rgb_d, rgb_cam0;
-    rgb_cam0 = cv::Mat(camera_0.getHeight(), camera_0.getWidth(), CV_8UC4, 1);
-
-    std::cout << "Press 's' to save images" << std::endl;
-    char key = ' ';
-    int image_count = 0;
-    while (key != 'q') {
-      if (camera_0.isNewFrame()) {
-
-        memcpy(rgb_cam0.data, camera_0.getPixels(),
-               camera_0.getWidth() * camera_0.getHeight() *
-                   camera_0.getNumberOfChannels());
-        /// Only with argus ///
-       // std::cout << " Exposure Time : " << camera_0.getFrameExposureTime()
-       //           << " us" << std::endl;
-       // std::cout << " Analog Gain : " << camera_0.getAnalogFrameGain() << " dB"
-       //           << std::endl;
-       // std::cout << " Digital Gain : " << camera_0.getDigitalFrameGain()
-       //           << std::endl;
-       ///////////////////
-
-        cv::resize(rgb_cam0, rgb_d, cv::Size(1280, 720));
-        // saves the images
-        cv::imwrite("image_left_" + std::to_string(image_count) + ".png",
-                    rgb_cam0);
-        std::cout << "images created." << std::endl;
-        image_count++;
-      }
-      else
-        usleep(100);
+    
+    std::cout << "Found " << available_camera_ids.size() << " available cameras" << std::endl;
+    
+    // Create threads for each camera
+    std::vector<std::thread> camera_threads;
+    for (int camera_id : available_camera_ids) {
+        std::cout << "Starting thread for camera " << camera_id << std::endl;
+        camera_threads.emplace_back(cameraThread, camera_id, rq_width, rq_height, rq_fps);
     }
-    camera_0.closeCamera();
+    
+    std::cout << "All camera threads started. Press Ctrl+C to exit." << std::endl;
+    
+    // Wait for all threads to complete
+    for (auto& thread : camera_threads) {
+        thread.join();
+    }
+    
+    std::cout << "All camera threads have completed." << std::endl;
 
     return 0;
-
 }
